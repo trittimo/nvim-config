f2324166b9ec35e009dd18b4f12f14c7b20bd9cb
Michael Trittin <michaeltrittin@gmail.com>
2025-06-07 17:00:59 -0400

    Fix shiftwidth

--- lua/misc.lua
+++ lua/misc.lua
@@ -3,6 +3,7 @@ vim.g.mapleader = " "
 
 -- Use 4 spaces for tab
 vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 -- Show whitespace
@@ -13,3 +14,5 @@ vim.cmd(":set path+=**")
 -- :help vim.opt
 vim.cmd(":colorscheme slate")
 vim.cmd(":set relativenumber")
+
+vim.o.tags = "./tags;,tags"
