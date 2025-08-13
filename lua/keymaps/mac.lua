b68db4bf7cfebf79ab225899ec3ed4dbf16d0ab5
Michael Trittin <michaeltrittin@gmail.com>
2025-08-13 16:55:20 -0400

    Updates

--- lua/keymaps/mac.lua
+++ lua/keymaps/mac.lua
@@ -4,6 +4,29 @@ if vim.g.neovide then
     vim.keymap.set('c', '<D-v>', '<C-R>"+p') -- Paste command mode
     vim.keymap.set('i', '<D-v>', '<Esc>"+gpa') -- Paste insert mode
     vim.keymap.set("t", '<D-v>', '<C-\\><C-n>l"+gpa') -- Paste terminal mode
+
+    -- Increase font size
+    vim.keymap.set('n', '<D-=>', function()
+        local current_font = vim.o.guifont
+        local _, _, size = current_font:find(':h(%d+)')
+        size = tonumber(size) or 20
+        vim.o.guifont = current_font:gsub(':h%d+', '') .. ':h' .. (size + 1)
+    end, { silent = true })
+
+    -- Decrease font size
+    vim.keymap.set('n', '<D-->', function()
+        local current_font = vim.o.guifont
+        local _, _, size = current_font:find(':h(%d+)')
+        size = tonumber(size) or 20
+        if size > 6 then  -- Prevent font from getting too small
+            vim.o.guifont = current_font:gsub(':h%d+', '') .. ':h' .. (size - 1)
+        end
+    end, { silent = true })
+
+    -- Reset font size
+    vim.keymap.set('n', '<D-0>', function()
+        vim.o.guifont = 'Menlo:h20'
+    end, { silent = true })
 end
 
 vim.keymap.set({"i", "n"}, "<D-/>", "gcc", { remap = true, silent = true})
